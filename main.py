6e67cbcec9b156a664a92410807fba264782638a
szapodea <szapodea@purdue.edu>
2025-06-03 18:46:03 -0700

    initial commit:

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+This project will be a web scrapping tool initially scrapping the headlines of news sites.
+Once the headlines are gotten, they will be passed into a LLM model with carefully crafted prompts
+to determine characteristics about that article.
+
+The vague use case that inspired this project is given an article that mentions tariffs and Donald Trump,
+can a LLM determine if that article would be good or bad for stock market short-term?
+'''
+
+
+if __name__ == '__main__':
